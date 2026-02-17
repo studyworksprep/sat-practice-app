56872c26e845d0bea587dc3505da15c08cdbfa57
studyworksprep <julio@studyworksprep.com>
2026-02-17 08:25:05 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,17 @@
+import "../styles/globals.css";
+
+export const metadata = {
+  title: "SAT Practice",
+  description: "SAT Question Practice App"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="container">{children}</div>
+      </body>
+    </html>
+  );
+}
 
